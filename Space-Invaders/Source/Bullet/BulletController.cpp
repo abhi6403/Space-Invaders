a8e629fcb68451b1bc3419cbc855f6bd20c13c14
abhi6403 <163630847+abhi6403@users.noreply.github.com>
2024-08-06 13:12:06 +0530

    create Bullet MVC

---
+++ Space-Invaders/Source/Bullet/BulletController.cpp
@@ -0,0 +1,34 @@
+#include"../../Header/Bullet/BulletController.h"
+
+namespace Bullet
+{
+	BulletController::BulletController(BulletType type)
+	{
+
+	}
+
+	BulletController::~BulletController()
+	{
+
+	}
+
+	void BulletController::initialize()
+	{
+
+	}
+
+	void BulletController::getProjectilePosition()
+	{
+
+	}
+
+	void BulletController::render()
+	{
+		
+	}
+
+	void BulletController::update()
+	{
+
+	}
+}
